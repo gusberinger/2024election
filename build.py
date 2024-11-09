10ec626a42b2df2466b065e1630e5dd8c13bdd77
Gus Beringer <gusberinger@gmail.com>
2024-11-09 14:20:39 -0600

    Add Trump votes to tooltip

--- build.py
+++ build.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import pandas as pd
 import folium
 import json
+from tqdm import tqdm
 
 build_folder = Path(__file__).parent / "build"
 build_folder.mkdir(exist_ok=True)
@@ -38,7 +39,7 @@ geojson_path = Path(__file__).parent / "mn-precincts.json"
 geojson = json.loads(geojson_path.read_text())
 
 to_delete = []
-for feature in geojson["features"]:
+for feature in tqdm(geojson["features"]):
     county_id = int(feature["properties"]["CountyID"])
     county_id = f"{county_id:02d}"
     vtdid = feature["properties"]["PrecinctID"]
@@ -47,11 +48,14 @@ for feature in geojson["features"]:
     new_code = f"{county_id}{precinct_code}"
     feature["properties"]["PrecinctCountyCode"] = new_code
 
-    election_data = election_filtered[election_filtered["PrecinctCountyCode"] == new_code].iloc[0]
-    feature["properties"]["Total"] = int(election_data["Total"])
-    feature["properties"]["Votes"] = int(election_data["Votes"])
-    feature["properties"]["Perc"] = float(election_data["Perc"])
-    if election_data.Total == 0:
+    harris_data = election[(election["PrecinctCountyCode"] == new_code) & (election["Party"] == "DFL")].iloc[0]
+    trump_data = election[(election["PrecinctCountyCode"] == new_code) & (election["Party"] == "R")].iloc[0]
+    feature["properties"]["Total"] = int(harris_data["Total"])
+    feature["properties"]["HarrisPerc"] = float(harris_data["Perc"])
+    feature["properties"]["HarrisVotes"] = int(harris_data["Votes"])
+    feature["properties"]["TrumpPerc"] = float(trump_data["Perc"])
+    feature["properties"]["TrumpVotes"] = int(trump_data["Votes"])
+    if harris_data.Total == 0:
         to_delete.append(feature)
 
 for feature in to_delete:
@@ -75,8 +79,8 @@ choropleth = folium.Choropleth(
 geojson_layer = folium.GeoJson(
     geojson,
     tooltip=folium.GeoJsonTooltip(
-        fields=['PrecinctCountyCode', 'Votes', 'Perc', 'Total'],
-        aliases=['Precinct Code:', 'DFL Votes:', 'DFL %:', 'Total Votes:'],
+        fields=['PrecinctCountyCode', 'HarrisVotes', 'TrumpVotes', 'HarrisPerc', 'TrumpPerc', 'Total'],
+        aliases=['Precinct Code:', 'Harris Votes:', 'Trump Votes:', 'Harris %:', 'Trump %:', 'Total Votes:'],
         localize=True
     ),
     style_function=lambda x: {
