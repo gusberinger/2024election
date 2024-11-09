96442ceceff5f1c5b2074fd1681b39978d95cc5f
Gus Beringer <gusberinger@gmail.com>
2024-11-08 18:55:55 -0600

    Add choropleth map with geojson

---
+++ electionMap.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+import pandas as pd
+import folium
+import json
+
+election = pd.read_csv(
+    "USPresPct.txt", 
+    delimiter=";",
+    names=[
+        "State",
+        "CountyID",
+        "PrecinctID",
+        "OfficeID",
+        "OfficeName",
+        "NA1",
+        "COC",
+        "CandidateName",
+        "NA2",
+        "NA3",
+        "Party",
+        "NA4",
+        "NA5",
+        "Votes",
+        "Perc",
+        "Total"
+    ],
+)
+election['PrecinctCountyCode'] = (
+    election['CountyID'].astype(str).str.zfill(2) + 
+    election['PrecinctID'].astype(str).str.zfill(4)
+)
+election_filtered = election[election["Party"] == "DFL"]
+
+
+geojson_path = Path(__file__).parent / "mn-precincts.json"
+geojson = json.loads(geojson_path.read_text())
+
+for feature in geojson["features"]:
+    county_id = int(feature["properties"]["CountyID"])
+    county_id = f"{county_id:02d}"
+    vtdid = feature["properties"]["PrecinctID"]
+    precinct_code = vtdid[-4:]
+
+    new_code = f"{county_id}{precinct_code}"
+    feature["properties"]["PrecinctCountyCode"] = new_code
+
+
+m = folium.Map(location=[46.2, -93.093124], zoom_start=7)
+folium.GeoJson(geojson).add_to(m)
+
+choropleth = folium.Choropleth(
+    geo_data=geojson,
+    data=election_filtered,
+    columns=['PrecinctCountyCode', 'Perc'],  # Replace 'PrecinctID' with the appropriate unique identifier
+    key_on='feature.properties.PrecinctCountyCode',
+    fill_color='RdBu',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    threshold_scale=[0, 20, 40, 60, 80, 100],
+    bins=2,
+    overlay=True,
+    legend_name='Perc (%)'
+)
+choropleth.add_to(m)
+
+m.save("map.html")
