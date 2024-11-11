7d7918ed6f34b47050b9f79cd7729127f712cae4
Gus Beringer <gusberinger@gmail.com>
2024-11-10 22:41:22 -0600

    Optimize build script

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ build_folder = Path(__file__).parent / "build"
 build_folder.mkdir(exist_ok=True)
 
 election = pd.read_csv(
-    "USPresPct.txt", 
+    "USPresPct.txt",
     delimiter=";",
     names=[
         "State",
@@ -26,80 +26,95 @@ election = pd.read_csv(
         "NA5",
         "Votes",
         "Perc",
-        "Total"
+        "Total",
     ],
 )
-election['PrecinctCountyCode'] = (
-    election['CountyID'].astype(str).str.zfill(2) + 
-    election['PrecinctID'].astype(str).str.zfill(4)
+election["PrecinctCountyCode"] = election["CountyID"].astype(str).str.zfill(2) + election["PrecinctID"].astype(str).str.zfill(4)
+
+harris_lookup = (
+    election[election["Party"] == "DFL"]
+    .set_index("PrecinctCountyCode")
+    .to_dict(orient="index")
 )
-election_filtered = election[(election["Party"] == "DFL")]
+trump_lookup = (
+    election[election["Party"] == "R"]
+    .set_index("PrecinctCountyCode")
+    .to_dict(orient="index")
+)
 
 geojson_path = Path(__file__).parent / "mn-precincts.json"
 geojson = json.loads(geojson_path.read_text())
 
 to_delete = []
 for feature in tqdm(geojson["features"]):
-    county_id = int(feature["properties"]["CountyID"])
-    county_id = f"{county_id:02d}"
-    vtdid = feature["properties"]["PrecinctID"]
-    precinct_code = vtdid[-4:]
-
+    county_id = f"{int(feature['properties']['CountyID']):02d}"
+    precinct_code = feature["properties"]["PrecinctID"][-4:]
     new_code = f"{county_id}{precinct_code}"
     feature["properties"]["PrecinctCountyCode"] = new_code
 
-    harris_data = election[(election["PrecinctCountyCode"] == new_code) & (election["Party"] == "DFL")].iloc[0]
-    trump_data = election[(election["PrecinctCountyCode"] == new_code) & (election["Party"] == "R")].iloc[0]
-    feature["properties"]["Total"] = int(harris_data["Total"])
-    feature["properties"]["HarrisPerc"] = float(harris_data["Perc"])
-    feature["properties"]["HarrisVotes"] = int(harris_data["Votes"])
-    feature["properties"]["TrumpPerc"] = float(trump_data["Perc"])
-    feature["properties"]["TrumpVotes"] = int(trump_data["Votes"])
-    
-    del feature["properties"]["PrecinctID"]
-    del feature["properties"]["County"]    
-    del feature["properties"]["CountyID"]
-    del feature["properties"]["CongDist"]
-    del feature["properties"]["MNSenDist"]
-    del feature["properties"]["MNLegDist"]
-    del feature["properties"]["CtyComDist"]
-    
-    if harris_data.Total == 0:
+    harris_data = harris_lookup.get(new_code)
+    trump_data = trump_lookup.get(new_code)
+
+    if harris_data and trump_data:
+        feature["properties"]["Total"] = int(harris_data["Total"])
+        feature["properties"]["HarrisPerc"] = float(harris_data["Perc"])
+        feature["properties"]["HarrisVotes"] = int(harris_data["Votes"])
+        feature["properties"]["TrumpPerc"] = float(trump_data["Perc"])
+        feature["properties"]["TrumpVotes"] = int(trump_data["Votes"])
+    else:
         to_delete.append(feature)
+
+    for prop in [
+        "PrecinctID",
+        "County",
+        "CountyID",
+        "CongDist",
+        "MNSenDist",
+        "MNLegDist",
+        "CtyComDist",
+    ]:
+        feature["properties"].pop(prop, None)
 
 for feature in to_delete:
     geojson["features"].remove(feature)
 
 m = folium.Map(location=[46.2, -93.093124], zoom_start=7)
-
-
 choropleth = folium.Choropleth(
     geo_data=geojson,
-    data=election_filtered,
-    columns=['PrecinctCountyCode', 'Perc'],
-    key_on='feature.properties.PrecinctCountyCode',
-    fill_color='RdBu',
+    data=election[election["Party"] == "DFL"],
+    columns=["PrecinctCountyCode", "Perc"],
+    key_on="feature.properties.PrecinctCountyCode",
+    fill_color="RdBu",
     fill_opacity=0.7,
     bins=10,
     overlay=True,
-    legend_name='Percentage of Vote Won by DFL (%)'
+    legend_name="Percentage of Vote Won by DFL (%)",
 )
-
 geojson_layer = folium.GeoJson(
     geojson,
     tooltip=folium.GeoJsonTooltip(
-        fields=['Precinct', 'HarrisVotes', 'TrumpVotes', 'HarrisPerc', 'TrumpPerc', 'Total'],
-        aliases=['Precinct:', 'Harris Votes:', 'Trump Votes:', 'Harris %:', 'Trump %:', 'Total Votes:'],
-        localize=True
+        fields=[
+            "PrecinctCountyCode",
+            "HarrisVotes",
+            "TrumpVotes",
+            "HarrisPerc",
+            "TrumpPerc",
+            "Total",
+        ],
+        aliases=[
+            "Precinct:",
+            "Harris Votes:",
+            "Trump Votes:",
+            "Harris %:",
+            "Trump %:",
+            "Total Votes:",
+        ],
+        localize=True,
     ),
-    style_function=lambda x: {
-        'fillOpacity': 0,  # Transparent fill so it won't override the Choropleth
-        'color': 'transparent'  # Transparent border
-    }
+    style_function=lambda x: {"fillOpacity": 0, "color": "transparent"},
 )
 
 choropleth.add_to(m)
 geojson_layer.add_to(m)
-
 
 m.save(build_folder / "index.html")
